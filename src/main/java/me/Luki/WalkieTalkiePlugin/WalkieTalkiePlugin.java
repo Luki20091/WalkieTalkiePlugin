f2563e7b09224637b0f71ec65c2238328ca0e312
Łukasz Kucharczyk <lukaszkucharczyk2009@gmail.com>
2025-12-22 19:13:46 +0100

    build

---
+++ src/main/java/me/Luki/WalkieTalkiePlugin/WalkieTalkiePlugin.java
@@ -0,0 +1,17 @@
+package me.Luki.WalkieTalkiePlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WalkieTalkiePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
